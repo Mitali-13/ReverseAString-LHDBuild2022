a3840ea63b670c53815881344e4426a1d06f24f9
Mitali Garg <72097687+Mitali-13@users.noreply.github.com>
2022-01-11 14:58:40 +0530

    program file added
    holds the code to reverse a string using c#

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace ReverseString
+{
+    class Program
+    {
+        public static void Main(String [] args)
+        {
+            Console.WriteLine("Enter string to be reversed:");
+            string input = Console.ReadLine();
+
+            string reversedString = "";
+
+            for(int i = 0 ; i<input.Length ; i++)
+            {
+                string charAti = Char.ToString(input[i]);
+                reversedString = charAti + reversedString;
+            }
+
+             Console.WriteLine(reversedString);
+
+             char[] inputarray = input.ToCharArray();
+
+             for(int i=0,j=input.Length-1 ; i<j ;i++,j--)
+             {
+                 char temp = inputarray[i];
+                 inputarray[i] = inputarray[j];
+                 inputarray[j] = temp;
+             }    
+             
+             reversedString = new String(inputarray);
+             Console.WriteLine(reversedString);    
+        }    
+
+    }
+}
